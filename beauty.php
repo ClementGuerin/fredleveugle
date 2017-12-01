28403b55f1cd3b8686652be4ab24c7a420cebdee
Mathieu Blok <33958431+MatttBlok@users.noreply.github.com>
2017-12-01 18:21:18 +0100

    Add files via upload

---
+++ beauty.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8"/>
+   <link rel="stylesheet" href="css/style.css"/>
+   <link rel="stylesheet" href="css/resset.css"/>
+   <link rel="stylesheet" href="css/dist/css/animsition.min.css">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet" />
+   <title>Project</title>
+</head>
+
+<body class="animsition">
+<div id="header">
+      <div class="left">
+      <p><a href="index.php"><strong>FRED LEVEUGLE</strong> </a><span>BEAUTY PHOTOGRAPHIE</span></p>
+   </div>
+
+   <div class="right">
+      <a href="#"><img class="menu-logo" src="img/icon/menu.png"/></a>
+   </div>
+</div>
+
+   <div class="wrapper">
+      <section class="left-colonne">
+         <img src="img/girls/1.jpg" class="img-responsive"><br><span>NAME 01</span></img>
+         <img src="img/girls/beauty/1.jpg" class="img-responsive"><br><span>NAME 01</span></img>
+         <img src="img/girls/beauty/10.jpg" class="img-responsive"><br><span>NAME 01</span></img>
+      </section>
+
+      <section class="middle-colonne">
+         <img src="img/girls/beauty/2.jpg" class="img-responsive"><br><span>NAME 01</span></img>
+         <img src="img/girls/beauty/8.jpg" class="img-responsive"><br><span>NAME 01</span></img>
+         <img src="img/girls/beauty/9.jpg" class="img-responsive"><br><span>NAME 01</span></img>
+         <img src="img/girls/beauty/3.jpg" class="img-responsive"><br><span>NAME 01</span></img>
+      </section>
+
+      <section class="right-colonne">
+         <img src="img/girls/beauty/6.jpg" class="img-responsive"><br><span>NAME 01</span></img>
+         <img src="img/girls/beauty/9.jpg" class="img-responsive"><br><span>NAME 01</span></img>
+         <img src="img/girls/beauty/4.jpg" class="img-responsive"><br><span>NAME 01</span></img>
+      </section>
+   </div>
+   <a class="js-scrollTo" href="#header"><img class="arrow-up" src="img/icon/arrow_up.svg"  alt="arrow"/></a>
+
+   <div class="burger-page">
+      <a href="#"><img class="menu-logo-close" src="img/icon/cancel.png"/></a>
+      <div class="content-burger-page">
+
+         <div class="category">
+            <ul>
+               <li><a href="#">Beauty</a></li>
+               <li><a href="#">Jewellery</a></li>
+               <li><a href="#">Advertising</a></li>
+            </ul>
+         </div>
+
+         <div class="category">
+            <ul>
+               <li><a href="#">Fashion</a></li>
+               <li><a href="#">Personnal</a></li>
+               <li><a href="#">Celebrities</a></li>
+            </ul>
+         </div>
+
+         <div class="category">
+            <ul>
+               <li><a href="#">Video</a></li>
+               <li><a href="#">Lingerie</a></li>
+               <li><a href="#">Contact</a></li>
+            </ul>
+         </div>
+
+      </div>
+</body>
+
+   <script src="js/jquery.min.js"></script>
+   <script src="css/dist/js/animsition.min.js"></script>
+   <script src="js/script.js"></script>
+   <script src="js/scroll.js"></script>
+
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8"/>
+   <link rel="stylesheet" href="css/style.css"/>
+   <link rel="stylesheet" href="css/resset.css"/>
+   <link rel="stylesheet" href="css/dist/css/animsition.min.css">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   <link href="https://fonts.googleapis.com/css?family=Roboto:300" rel="stylesheet">
+
+   <title>Fred Leveugle Portfolio</title>
+</head>
+
+    <?php include("home.php"); ?>
+
+
+   <script src="js/jquery.min.js"></script>
+   <script src="css/dist/js/animsition.min.js"></script>
+   <script src="js/script.js"></script>
+   <script src="js/scroll.js"></script>
+
+
+</body>
+</html>
